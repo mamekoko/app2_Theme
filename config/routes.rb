8b3cec3d09780588a572e8475422b8c683abbfc5
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-05-18 07:39:09 +0000

    bookers2_config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  get 'usres/edit'
+  
   root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
+  
   devise_for :users
   
   
+  resources :books, only: [:index, :show, :edit]
   
+  resources :users, only: [:edit]
+  
+  
+  get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
