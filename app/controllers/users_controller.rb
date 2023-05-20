8bc849f3b9c964eb351e0c5e0cf23eca3fcc7ebd
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-05-20 18:32:22 +0000

    bookers2_app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def index
+    @user = current_user
   end
 
   def show
@@ -8,7 +9,13 @@ class UsersController < ApplicationController
 
   def edit
   end
-  
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
   private
 
   def user_params
