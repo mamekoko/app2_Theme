c273b5e401e6b594ecbde8291965e1bb29a4b20c
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-05-20 15:59:56 +0000

    bookers2_app

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,18 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
-    if @book.save
-    redirect_to book_path(@book.id)
-    end
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+    
   end
 
   def show
+    @book = Book.find(params[:id])
   end
   
   def edit
+    @book = Book.find(params[:])
   end
   
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,12 @@ class UsersController < ApplicationController
 
   def edit
   end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+
 
 end
