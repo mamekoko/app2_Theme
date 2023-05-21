85d203f36ba636aff90f10d32b702597d21aab75
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-05-21 12:28:40 +0000

    bookers2_app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,11 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    users_path
+    users_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+     root_path
   end
 
 
