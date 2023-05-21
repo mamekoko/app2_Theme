c6164305166c07dc5e9fd5f2320c8432b23b3eb8
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-05-21 13:07:33 +0000

    bookers2_app

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ApplicationRecord
-  
-  has_one_attached :image
+
+  validates :title, presence: true
+  validates :body, presence: true
+
+  has_one_attached :profile_image
   belongs_to :user
-  
+
 end
